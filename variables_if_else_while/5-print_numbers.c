a9f482b59ebf8480f3146adc043b704d5fb8da6d
Fariidismail <feridismayilov1995@gmail.com>
2024-02-20 02:18:28 -0800

    updated

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * This code prints number from 0 to 9
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void){
+	printf("0123456789\n");
+	return (0);
+}
