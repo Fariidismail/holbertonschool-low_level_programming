a5b42099e55cd411298aedcdb0b42f1708c1b36c
Fariidismail <feridismayilov1995@gmail.com>
2024-02-20 03:29:52 -0800

    updated

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * This function generates a random number and determines if it's positive,
+ * negative, or zero.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char lowercase = 'z';
+	while (lowercase >= 'a')
+	{
+		putchar(lowercase);
+		lowercase--;
+	}
+	putchar('\n');
+	return (0);
+}
