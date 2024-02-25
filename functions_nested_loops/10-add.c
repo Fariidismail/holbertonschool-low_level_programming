bafa983fe47acd4c782893411ece6e65b566ec2b
Fariidismail <feridismayilov1995@gmail.com>
2024-02-25 09:07:59 +0000

    updated

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -2,15 +2,12 @@
 #include <stdio.h>
 
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * add - additon
+ * @num1: number one
+ * @num2: number two
+ * Return: num1+num2
  */
-int main(void)
+int add(int num1, int num2)
 {
-	int n;
-
-	n = add(89, 9);
-	printf("%d\n", n);
-	return (0);
+	return (num1 + num2);
 }
