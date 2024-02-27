c9f11a88a7d06e315ecb62712c3f10e958be1e67
Fariidismail <feridismayilov1995@gmail.com>
2024-02-27 15:38:01 +0000

    updated

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include <main.h>
+/*
+ * main - entry point
+ *
+ * Return 0 : Sucesses
+ */
+void print_numbers(void)
+{
+int i;
+
+for (i = 0; i <= 9; i++)
+{
+	_putchar(i + 48);
+}
+	_putchar('\n');
+}
