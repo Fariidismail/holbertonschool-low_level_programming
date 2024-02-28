3402fb08aa9ae9e1da584f2aad783ae5ab29fe0b
Fariidismail <feridismayilov1995@gmail.com>
2024-02-28 12:56:58 +0000

    updated

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -1,31 +1,34 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
-*main- prints the largest prime factor
-*of a number
-*
-*Return: returns 0
-*/
+  * print_triangle - Prints a triangle of squares according parameter
+  * @size: The size of the squares triangle
+  *
+  * Return: empty
+  */
+void print_triangle(int size)
+{
+	int x, y, z;
 
-int main(void)
-{
-long number = 612852475143;
-int inc;
-while (inc++ < number / 2)
-{
-if (number % inc == 0)
-{
-number /= 2;
-continue;
-}
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (x = 0; x < size; x++)
+		{
+			for (y = size - x; y > 1; y--)
+			{
+				_putchar(32);
+			}
 
-for (inc = 3; inc < number / 2; inc += 2)
-{
-if (number % inc == 0)
-number /= inc;
+			for (z = 0; z <= x; z++)
+			{
+				_putchar(35);
+			}
 
-}
-}
-printf("%ld\n", number);
-return (0);
+			_putchar('\n');
+		}
+	}
 }
