dc99d70b90c6c62a0817b65e81f37873da213c82
Fariidismail <feridismayilov1995@gmail.com>
2024-02-29 06:28:12 +0000

    updated

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _strlen - length of string
+ * @a : value
+ */
+int _strlen(char *s)
+{
+	int counter=0;
+
+	while (s[counter] != '\0') 
+	{
+        	counter++;
+    	}
+	return counter;
+}
