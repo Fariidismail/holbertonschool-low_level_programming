fd38f936ebb0c5ab769169a8aab77001669fb523
Fariidismail <feridismayilov1995@gmail.com>
2024-03-08 13:13:44 +0000

    updated

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -4,10 +4,17 @@
  * @s : string
  * return : length of string
  */
-
 int _strlen_recursion(char *s)
 {
-if (*s == '\0')
-	return 0;
-return 1 + _strlen_recursion(s + 1);
+	int counter = 1;
+
+	if (*s != '\0')
+	{
+		counter += _strlen_recursion(s + 1);
+		return (counter);
+	}
+	else
+	{
+		return (0);
+	}
 }
