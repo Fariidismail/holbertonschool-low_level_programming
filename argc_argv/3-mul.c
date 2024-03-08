28d09b4ca3fe56b2d26c7eab594d8f6470e15320
Fariidismail <feridismayilov1995@gmail.com>
2024-03-08 16:29:36 +0000

    updated

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - main
+ * @argc: arguments count
+ * @argv: pointer
+ *
+ * Return: int
+ */
+int main(int argc, char *argv[])
+{
+		int result;
+		(void)argc;
+
+		if (argv[1] != NULL && argv[2] != NULL)
+		{
+			result = atoi(argv[1]) * atoi(argv[2]);
+			printf("%d\n", result);
+		}
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
+
+		return (0);
+}
