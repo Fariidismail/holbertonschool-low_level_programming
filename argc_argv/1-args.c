a28d89689eaf281792dc262c2b2d61c2e17d2cbe
Fariidismail <feridismayilov1995@gmail.com>
2024-03-08 16:24:08 +0000

    updated

---
+++ argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - main
+ * @argc: arguments count
+ * @argv: pointer
+ *
+ * Return: int
+ */
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argc - 1);
+
+	return (0);
+}
