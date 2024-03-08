dfe5e3d62f52f5244a09fb69b55df3f125c8324a
Fariidismail <feridismayilov1995@gmail.com>
2024-03-08 16:17:52 +0000

    updated

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - main
+ * @argc: arguments count
+ * @argv: pointer
+ *
+ * Return: int
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
