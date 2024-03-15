7a9f08e1b6410289a2384cc177378b659354d7ae
Fariidismail <feridismayilov1995@gmail.com>
2024-03-15 09:20:47 +0000

    updated

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * malloc_checked - check whether malloc or not
+ * @b - unsigned int
+ *
+ * Return : void
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *ptr = malloc(b);
+
+	if (ptr == NULL)
+		exit(98);
+
+	return (ptr);
+}
