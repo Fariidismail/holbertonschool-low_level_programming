3ecef5e4e09da35e86e6c8611459d65c24ba9fed
Fariidismail <feridismayilov1995@gmail.com>
2024-03-15 12:38:30 +0000

    updated

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,7 +1,13 @@
 #ifndef DOG_H
 #define DOG_H
+/**
+ * struct dog - Represents information about a dog.
+ * @name: The name of the dog.
+ * @age: The age of the dog.
+ * @owner: The owner of the dog.
+ */
 
-struct dog {
+typedef struct dog {
     char *name;
     float age;
     char *owner;
