f212b96b7ef29f066bb587de120849517cd624bc
Fariidismail <feridismayilov1995@gmail.com>
2024-04-02 13:28:50 +0000

    updated

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+ * print_list - print list
+ * @h: list_t type node
+ *
+ * Return: unsigned int
+ */
+size_t print_list(const list_t *h)
+{
+	unsigned int counter = 0;
+
+	while (h != NULL)
+	{
+		counter++;
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+			h = h->next;
+		}
+		else
+		{
+			printf("[%u] %s\n", h->len, h->str);
+			h = h->next;
+		}
+	}
+	return (counter);
+}
---
+++ singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+singliy Linked list
