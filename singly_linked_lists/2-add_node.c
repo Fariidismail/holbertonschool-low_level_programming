6790ef49ff90abdbf8bd33206f0d4d9d7d44d4e2
Fariidismail <feridismayilov1995@gmail.com>
2024-04-04 15:29:19 +0400

    updated

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -1,35 +1,36 @@
-#include "lists.h"
-#include <string.h>
 #include <stdio.h>
-
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
 /**
- * add_node - add a new node at the beginning of `list_t` list
- * @head: double pointer to head
- * @str: string to be saved in new node, must be duplicated
- * Return: Address of new element or NULL if failed
+ * add_node - ad node to the first index of list
+ * @head: list_t type node
+ * @str: string
+ *
+ * Return: ptr
  */
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *new_node;
-	int c;
-
-	for (c = 0; dup_str[c] != '\0'; c++)
-		;
-	new_node = malloc(sizeof(list_t));
-	if (new_node == NULL)
+	if (*head == NULL)
 	{
-		free(new_node);
-		return (NULL);
+		*head = (list_t *)malloc(sizeof(list_t));
+		if (*head == NULL)
+		{
+			free(*head);
+			return (NULL);
+		}
+		(*head)->str = strdup(str);
+		(*head)->len = strlen(str);
+		(*head)->next = NULL;
 	}
-	new_node->str = strdup(str);
-	if (new_node->str == NULL)
+	else
 	{
-		free(new_node);
-		return (NULL);
-	}
-	new_node->len = c;
-	new_node->next = *head;
+		list_t *new_node = (list_t *)malloc(sizeof(list_t));
 
-	*head = new_node;
+		new_node->str = strdup(str);
+		new_node->len = strlen(str);
+		new_node->next = *head;
+		*head = new_node;
+	}
 	return (*head);
 }
